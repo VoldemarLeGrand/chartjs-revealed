e6cf084224e9d09c3377a197e2669c60c649532e
Voldemar <voldemarlegrand@gmail.com>
2015-12-12 20:17:30 +0100

    Loading Chart.js script from CDN or from URL specified in Reveal.chartjs.libUrl

--- plugin/chartjs/chartjs.js
+++ plugin/chartjs/chartjs.js
@@ -1,8 +1,70 @@
 /**
  * A plugin to visualise chart.js charts inside of reveal.js slides.
  *
+ * With some functions borrowed from math.js plugin for reveal.js by Hakim El Hattab
+ *
  * @author Voldemar Le Grand <voldemarlegrand@gmail.com>
  */
 var RevealChartjs = window.RevealChartjs || (function(){
+
+    var options = Reveal.getConfig().chartjs || {};
+    options.libUrl = options.libUrl || 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.js';
+
+    loadScript(options.libUrl, function() {
+
+        var data = [
+            {
+                value: 300,
+                color:"#F7464A",
+                highlight: "#FF5A5E",
+                label: "Red"
+            },
+            {
+                value: 50,
+                color: "#46BFBD",
+                highlight: "#5AD3D1",
+                label: "Green"
+            },
+            {
+                value: 100,
+                color: "#FDB45C",
+                highlight: "#FFC870",
+                label: "Yellow"
+            }
+        ];
+        var ctx = document.getElementById("chart-example").getContext("2d");
+        var chart = new Chart(ctx);
+        var doughnut = chart.Doughnut(data, { })
+
+    });
+
+    function loadScript( url, callback ) {
+
+        var head = document.querySelector( 'head' );
+        var script = document.createElement( 'script' );
+        script.type = 'text/javascript';
+        script.src = url;
+
+        // Wrapper for callback to make sure it only fires once
+        var finish = function() {
+            if( typeof callback === 'function' ) {
+                callback.call();
+                callback = null;
+            }
+        }
+
+        script.onload = finish;
+
+        // IE
+        script.onreadystatechange = function() {
+            if ( this.readyState === 'loaded' ) {
+                finish();
+            }
+        }
+
+        // Normal browsers
+        head.appendChild( script );
+
+    }
 
 })();
