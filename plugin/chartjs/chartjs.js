e0c05906751a99a24272a749240746b52a6be9df
Voldemar <voldemarlegrand@gmail.com>
2015-12-12 20:37:10 +0100

    Using head.js 0.96 from Reveal package to load external ressources instead of our own function

--- plugin/chartjs/chartjs.js
+++ plugin/chartjs/chartjs.js
@@ -1,7 +1,7 @@
 /**
  * A plugin to visualise chart.js charts inside of reveal.js slides.
  *
- * With some functions borrowed from math.js plugin for reveal.js by Hakim El Hattab
+ * Using head.js 0.96 from Reveal package to load external ressources.
  *
  * @author Voldemar Le Grand <voldemarlegrand@gmail.com>
  */
@@ -10,7 +10,7 @@ var RevealChartjs = window.RevealChartjs || (function(){
     var options = Reveal.getConfig().chartjs || {};
     options.libUrl = options.libUrl || 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.js';
 
-    loadScript(options.libUrl, function() {
+    head.js(options.libUrl, function() {
 
         var data = [
             {
@@ -37,34 +37,5 @@ var RevealChartjs = window.RevealChartjs || (function(){
         var doughnut = chart.Doughnut(data, { })
 
     });
-
-    function loadScript( url, callback ) {
-
-        var head = document.querySelector( 'head' );
-        var script = document.createElement( 'script' );
-        script.type = 'text/javascript';
-        script.src = url;
-
-        // Wrapper for callback to make sure it only fires once
-        var finish = function() {
-            if( typeof callback === 'function' ) {
-                callback.call();
-                callback = null;
-            }
-        }
-
-        script.onload = finish;
-
-        // IE
-        script.onreadystatechange = function() {
-            if ( this.readyState === 'loaded' ) {
-                finish();
-            }
-        }
-
-        // Normal browsers
-        head.appendChild( script );
-
-    }
 
 })();
