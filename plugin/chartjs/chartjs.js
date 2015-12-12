a64068d8f16d3397a149ca9e44fc3bd55d5f445a
Voldemar <voldemarlegrand@gmail.com>
2015-12-12 19:40:15 +0100

    initial import

---
+++ plugin/chartjs/chartjs.js
@@ -0,0 +1,8 @@
+/**
+ * A plugin to visualise chart.js charts inside of reveal.js slides.
+ *
+ * @author Voldemar Le Grand <voldemarlegrand@gmail.com>
+ */
+var RevealChartjs = window.RevealChartjs || (function(){
+
+})();
