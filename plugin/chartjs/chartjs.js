aff3ad47547aaa6120be200fbe0e529237d5740c
Voldemar <voldemarlegrand@gmail.com>
2015-12-13 14:10:11 +0100

    Externalise chart drawing function

--- plugin/chartjs/chartjs.js
+++ plugin/chartjs/chartjs.js
@@ -13,31 +13,35 @@ var RevealChartjs = window.RevealChartjs || (function(){
     head.js(options.libUrl, function() {
         Reveal.addEventListener("slidechanged", function(event){
             if(event.currentSlide.id == "chart") {
-                var data = [
-                    {
-                        value: 300,
-                        color:"#F7464A",
-                        highlight: "#FF5A5E",
-                        label: "Red"
-                    },
-                    {
-                        value: 50,
-                        color: "#46BFBD",
-                        highlight: "#5AD3D1",
-                        label: "Green"
-                    },
-                    {
-                        value: 100,
-                        color: "#FDB45C",
-                        highlight: "#FFC870",
-                        label: "Yellow"
-                    }
-                ];
-                var ctx = document.getElementById("chart-example").getContext("2d");
-                var chart = new Chart(ctx);
-                var doughnut = chart.Doughnut(data, { })
+                drawChart(document.getElementById("chart-example"));
             }
-        });
+        })
     });
+
+    function drawChart(canvas) {
+        var data = [
+            {
+                value: 300,
+                color:"#F7464A",
+                highlight: "#FF5A5E",
+                label: "Red"
+            },
+            {
+                value: 50,
+                color: "#46BFBD",
+                highlight: "#5AD3D1",
+                label: "Green"
+            },
+            {
+                value: 100,
+                color: "#FDB45C",
+                highlight: "#FFC870",
+                label: "Yellow"
+            }
+        ];
+        var ctx = canvas.getContext("2d");
+        var chart = new Chart(ctx);
+        var doughnut = chart.Doughnut(data, { })
+    };
 
 })();
